5200e87cddd3f06021b677e3b43c4cbcaf27de80
jpatrick <castillo.josh7926@gmail.com>
2021-04-20 18:41:42 -0700

    adding a reusable button component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './button.scss'
+
+const Button = () => {
+  return(
+    <div className="button-cta">
+      <a href="#">
+        SHOP NOW
+      </a>
+    </div>
+
+  )
+}
+
+export default Button;
