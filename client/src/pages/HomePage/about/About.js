6c0cb82e95dd97ced6d9c7b1fce9340db75712b7
jpatrick7926 <castillo.josh7926@gmail.com>
2021-10-28 20:12:03 -0700

    changed about section in homepage / updated title and content in index.html

--- client/src/pages/HomePage/about/About.js
+++ client/src/pages/HomePage/about/About.js
@@ -13,28 +13,28 @@ const About = () => {
         <div className="col">
           <img src={"https://images.unsplash.com/photo-1577641785866-d40384a52ed5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"} alt="temporary alt text"/>
           <h3>
-            Subheader
+            Culture
           </h3>
           <p>
-            Making coffee at home or office shouldn’t feel like a compromise. We’ve engineered a pour over style system with the precision of high-end espresso machines. You’ll taste the difference.
+            The makers of J-COMM, Joshua Castillo and Juan Marin, are two hard working developers. Fascinated by the world of computer programming, these two are willing to go the extra measure to make your team stronger.
           </p>
         </div>
         <div className="col">
           <img src={"https://images.unsplash.com/photo-1585914622712-37ac29257b9a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"} alt="temporary alt text"/>
           <h3>
-            Subheader
+            Tech-Stack
           </h3>
           <p>
-            Making coffee at home or office shouldn’t feel like a compromise. We’ve engineered a pour over style system with the precision of high-end espresso machines. You’ll taste the difference.
+            J-COMM was made using the MERN stack. The front-end is built on React and Redux for state management. We also access "fakestoreapi.com" for the product and product information.
           </p>
         </div>
         <div className="col">
           <img src={"https://images.unsplash.com/photo-1614241819294-c0fe1b806943?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"} alt="temporary alt text"/>
           <h3>
-            Subheader
+            Culture
           </h3>
           <p>
-            Making coffee at home or office shouldn’t feel like a compromise. We’ve engineered a pour over style system with the precision of high-end espresso machines. You’ll taste the difference.
+            The makers of J-COMM, Joshua Castillo and Juan Marin, are two hard working developers. Fascinated by the world of computer programming, these two are willing to go the extra measure to make your team stronger.
           </p>
         </div>
       </div>
